866585412d97f832410f365b9aa025cb37ac629a
mabornea <mabornea@us.ibm.com>
2022-08-30 09:15:00 -0400

    Run  copy mechanism with KG oracle

--- primeqa/lfqa_kb/processors/preprocessors/eli5.py
+++ primeqa/lfqa_kb/processors/preprocessors/eli5.py
@@ -220,6 +220,110 @@ def preprocess_eli5_batch_cmsks_oracle(examples, data_args, mode="train") -> Tup
 
 
 
+
+### For CMSKS KG oracle ###
+def preprocess_eli5_function_cmsks_kg_oracle(examples, data_args, tokenizer, max_seq_length, max_answer_length, padding):
+    indices, inputs, targets, queries = preprocess_eli5_batch_cmsks_kg_oracle(examples, data_args, mode="train")
+    passage_ids, passage_masks = encode_passages(inputs, tokenizer, max_seq_length)
+    with tokenizer.as_target_tokenizer():
+        labels = tokenizer(targets, max_length=max_answer_length, padding=padding, truncation=True)
+    if padding == "max_length" and data_args.ignore_pad_token_for_loss:
+        labels["input_ids"] = [
+            [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
+        ]
+    model_inputs = {}
+    model_inputs["input_ids"] = passage_ids
+    model_inputs["attention_mask"] = passage_masks
+    model_inputs["labels"] = labels["input_ids"]
+    model_inputs["example_id"] = indices
+    model_inputs["query"] = queries
+    return model_inputs
+
+def preprocess_eli5_validation_function_cmsks_kg_oracle(examples, data_args, tokenizer, max_seq_length, max_answer_length, padding):
+    indices, inputs, targets, queries = preprocess_eli5_batch_cmsks_kg_oracle(examples, data_args, mode="eval")
+    passage_ids, passage_masks = encode_passages(inputs, tokenizer, max_seq_length)
+    with tokenizer.as_target_tokenizer():
+        labels = tokenizer(targets, max_length=max_answer_length, padding=padding, truncation=True)
+    if padding == "max_length" and data_args.ignore_pad_token_for_loss:
+        labels["input_ids"] = [
+            [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
+        ]
+    
+    model_inputs = {}
+    model_inputs["input_ids"] = passage_ids
+    model_inputs["attention_mask"] = passage_masks
+    model_inputs["labels"] = labels["input_ids"]
+    model_inputs["example_id"] = []
+    for i in range(len(model_inputs["input_ids"])):
+        model_inputs["example_id"].append(examples["id"][i])
+    model_inputs["query"] = queries
+    return model_inputs
+
+def preprocess_eli5_batch_cmsks_kg_oracle(examples, data_args, mode="train") -> Tuple[List[str], List[str]]:
+    '''
+    queries are oracle KG words: answer words
+    '''
+    questions = examples[data_args.question_column]
+    answers = examples[data_args.answer_column]
+    contexts = examples[data_args.context_column]
+    kg_vocabs = examples["kg_vocab"]
+    n_doc = data_args.n_context
+
+    def top_passages(ctx):
+        assert n_doc <= len(ctx) 
+        return [ctx[i]["text"] for i in range(n_doc)]
+    def append_question(passages, question):
+        return [f"question: {question} passage: {t}" for t in passages]
+    
+    # multiple answers for training
+    if mode == "train":
+        indices = []
+        inputs = []
+        targets = []
+        queries = [] # initial query for kg. It is a list of words made from the question
+        for idx,q in enumerate(questions):
+            passages = top_passages(contexts[idx])
+            question_passages = append_question(passages, q)
+            answer_list = answers[idx]
+            if len(answer_list) == 0:
+                inputs.append(question_passages)
+                targets.append("")  
+                indices.append(examples["id"][idx])
+                queries.append([])
+            else: # multiple answers
+                for answer_data in answer_list:
+                    a = answer_data["answer"]
+                    answer_score = answer_data["meta"]["score"]     
+                    if answer_score >= 3: # only takes answers whose score>3
+                        inputs.append(question_passages)
+                        targets.append(a)
+                        indices.append(examples["id"][idx])
+                        queries.append(kg_vocabs[idx]) # use answer to query
+                        # print(get_initial_query(a))
+                    
+    elif mode == "eval": # for evaluation only take each question once
+        inputs = []
+        indices = []
+        queries = []
+        for idx,q in enumerate(questions):
+            passages = top_passages(contexts[idx])
+            question_passages = append_question(passages, q)
+            inputs.append(question_passages)
+            indices.append(examples["id"][idx])
+            answer_list = answers[idx]
+            qry = kg_vocabs[idx]
+            print(examples["id"][idx], qry)
+            queries.append(qry)
+            
+        targets = [answer[0]["answer"] if len(answer) > 0 else "" for answer in answers]
+    else:
+        raise ValueError("mode requires eval or train")
+
+    return indices, inputs, targets, queries # inputs is a list of a list of question+passage, targets is a list of answers
+
+
+
+
 #### For FiD ####
 # from 
 # https://github.com/facebookresearch/FiD/blob/25ed1ff0fe0288b80fb5e9e5de8d6346b94b8d48/src/data.py#L73
