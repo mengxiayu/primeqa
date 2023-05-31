079ad4b08b15bcf4432c5f7a91fe8954bcaca8fe
Sara <sjrosenthal@us.ibm.com>
2023-05-31 14:50:06 -0400

    rouge updates

--- examples/lfqa_kb/compute_rouges.py
+++ examples/lfqa_kb/compute_rouges.py
@@ -5,12 +5,19 @@ from lfqa_kb.metrics.utils import _metric_max_over_ground_truths
 import pandas as pd
 
 def rouge(ref_data, prediction_file):
-    baseline_df = pd.read_json(prediction_file, lines=True, dtype=str) # orient='records' # lines=True
+    baseline_df = pd.read_json(prediction_file, lines=True, dtype=str)  #orient='records', lines=True
     baseline_df['hf_rouge'] = 0.0
     baseline_df['kilt_rouge'] = 0.0
 
     for i, row in baseline_df.iterrows():
-        hf_rouge_score, kilt_rouge_score = _metric_max_over_ground_truths(row["text"],ref_data[row["id"]]['output']) # prediction_text
+        answers = []
+        output = ref_data[row["id"]]['output']
+
+        for o in output:
+            if "answer" in o:
+                answers.append(o["answer"])
+
+        hf_rouge_score, kilt_rouge_score = _metric_max_over_ground_truths(row["text"],answers) # prediction_text
         
         baseline_df.loc[i, 'hf_rouge'] = hf_rouge_score
         baseline_df.loc[i, 'kilt_rouge'] = kilt_rouge_score
@@ -31,7 +38,7 @@ with open("/dccstor/srosent2/primeqa-mengxia/data/dpr-100passages_withkg_best_al
 
 
 import glob
-pred_files = glob.glob("/dccstor/srosent2/generative/baseline_llms/ELI5-full/512-limit/*/*/*/predictions*")
+pred_files = glob.glob("/dccstor/srosent2/generative/baseline_llms/ELI5-full/512-limit/closed-book/flan-t5-large/prefix_default-passages_False-0shot_pktemp-1.0_5_0.7-minmaxtok_100_1024/predictions-0-1507.json")
 
 for file in pred_files:
     hf_score, kilt_score = rouge(ELI5, file)
